c9640f55ecdb529eb657d1122d100796e8aa358c
mpisman <matti.pisman@ugent.be>
2018-05-30 13:20:58 +0200

    Organize files

---
+++ Scripts/Organize dataset.R
@@ -0,0 +1,6 @@
+#Load packages
+library(tidyverse)
+
+#Load raw dataset
+DataNatPun<-read.csv("D:\\OneDrive - UGent\\PhD wild pollinators\\Project Bijenindicator Natuurpunt\\Dataset kleurvallen plus transects.csv")
+levels(DataNatPun$Genus)
